c776d1af185f9eb31c1eeb3d6a025026bdeb5df5
rohaneth <rohanmuttepwar55@gmail.com>
2026-08-08 12:11:33 +0530

    fdsa

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import path from "path";
-
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const prismaClientSingleton = () => {
-  const dbPath = path.resolve(process.cwd(), "dev.db").replace(/\\/g, "/");
-  
-  const adapter = new PrismaLibSql({ url: `file:${dbPath}` });
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
   
   return new PrismaClient({ adapter });
 };
